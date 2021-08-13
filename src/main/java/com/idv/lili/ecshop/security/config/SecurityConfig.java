8bbdb13553ac424623d02959ae30e2bd59fb6352
loxa5210 <loxa5210@gmail.com>
2021-08-13 13:40:57 +0800

    add url

--- src/main/java/com/idv/lili/ecshop/security/config/SecurityConfig.java
+++ src/main/java/com/idv/lili/ecshop/security/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -32,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private UserDetailServiceImpl userDetailsService;
 
 	@Value("${viewUrl}")
-	private String viewUrl;
+	private List<String> viewUrl;
 
 	/**
 	 * AuthenticationManager 就是Spring Security用於執行身份驗證的組件，
@@ -86,7 +87,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList(viewUrl)); //or add * to allow all origins
+		configuration.setAllowedOrigins(viewUrl); //or add * to allow all origins
 		configuration.setAllowCredentials(true);
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); //to set allowed http methods
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
