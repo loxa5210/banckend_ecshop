bc6b98f27989447145786eda00d84243426c4cc7
loxa5210 <loxa5210@gmail.com>
2021-08-30 16:14:50 +0800

    移除測試程式

--- src/test/java/com/idv/lili/ecshop/EcShopApplicationTests.java
+++ src/test/java/com/idv/lili/ecshop/EcShopApplicationTests.java
@@ -1,21 +1,14 @@
 package com.idv.lili.ecshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 class EcShopApplicationTests {
 
-    @Value("${testUrl}")
-    List<String> viewUrl;
 
     @Test
     void contextLoads() {
-
-        System.err.println(viewUrl);
 
     }
 
