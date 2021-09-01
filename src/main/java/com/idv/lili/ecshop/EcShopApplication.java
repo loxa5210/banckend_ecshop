0ab6f1c05108950bfedee448f761a5925897f99c
loxa5210 <loxa5210@gmail.com>
2021-09-01 15:15:19 +0800

    調整讀取靜態檔方式

--- src/main/java/com/idv/lili/ecshop/EcShopApplication.java
+++ src/main/java/com/idv/lili/ecshop/EcShopApplication.java
@@ -3,14 +3,15 @@ package com.idv.lili.ecshop;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.idv.lili.ecshop.entity.Category;
-import com.idv.lili.ecshop.entity.Menu;
 import com.idv.lili.ecshop.entity.Goods;
+import com.idv.lili.ecshop.entity.Menu;
 import com.idv.lili.ecshop.entity.Users;
 import com.idv.lili.ecshop.repository.CategoryRepository;
-import com.idv.lili.ecshop.repository.MenuRepository;
 import com.idv.lili.ecshop.repository.GoodsRepository;
+import com.idv.lili.ecshop.repository.MenuRepository;
 import com.idv.lili.ecshop.repository.UsersRepository;
-import org.apache.commons.io.FileUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -21,67 +22,106 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class EcShopApplication {
 
-	@Value("classpath:static/category.json")
-	private Resource categoryResource;
+    @Value("classpath:static/category.json")
+    private Resource categoryResource;
 
-	@Value("classpath:static/menu.json")
-	private Resource menuResource;
+    @Value("classpath:static/menu.json")
+    private Resource menuResource;
 
-	@Value("classpath:static/goods.json")
-	private Resource goodsResource;
+    @Value("classpath:static/goods.json")
+    private Resource goodsResource;
 
-	@Value("classpath:static/users.json")
-	private Resource usersResource;
+    @Value("classpath:static/users.json")
+    private Resource usersResource;
 
-	@Autowired
-	private MenuRepository menuRepository;
+    @Autowired
+    private MenuRepository menuRepository;
 
-	@Autowired
-	private GoodsRepository goodsRepository;
+    @Autowired
+    private GoodsRepository goodsRepository;
 
-	@Autowired
-	private CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-	@Autowired
-	private UsersRepository usersRepository;
+    @Autowired
+    private UsersRepository usersRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcShopApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(EcShopApplication.class, args);
 
-	}
+    }
 
-	@EventListener(ApplicationReadyEvent.class)
-	private void initProjectData() throws IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
 
-		String categoryJSON = FileUtils.readFileToString(categoryResource.getFile(), "utf-8");
-		List<Category> categoryList = objectMapper.readValue(categoryJSON, new TypeReference<List<Category>>() {
-		});
-		categoryRepository.saveAll(categoryList);
+    private String getJsonData(String fileName) {
+        try {
+            InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("static/" + fileName);
+            if (null != resourceAsStream) {
+                return IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
+            }
+        } catch (IOException e) {
+            e.getStackTrace();
+            log.error(e+e.getMessage());
+        }
+        return null;
+    }
 
-		String menuJSON = FileUtils.readFileToString(menuResource.getFile(), "utf-8");
-		List<Menu> menuList = objectMapper.readValue(menuJSON, new TypeReference<List<Menu>>() {
-		});
-		menuRepository.saveAll(menuList);
+    @EventListener(ApplicationReadyEvent.class)
+    public void initProjectData() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
 
-		String goodsJSON = FileUtils.readFileToString(goodsResource.getFile(), "utf-8");
-		List<Goods> goodsList = objectMapper.readValue(goodsJSON, new TypeReference<List<Goods>>() {
-		});
+        String categoryJSON = getJsonData("category.json");
+        List<Category> categoryList = objectMapper.readValue(categoryJSON, new TypeReference<List<Category>>() {
+        });
+        categoryRepository.saveAll(categoryList);
 
-		goodsRepository.saveAll(goodsList);
+        String menuJSON = getJsonData("menu.json");
+        List<Menu> menuList = objectMapper.readValue(menuJSON, new TypeReference<List<Menu>>() {
+        });
+        menuRepository.saveAll(menuList);
 
-		String userJSON = FileUtils.readFileToString(usersResource.getFile(), "utf-8");
-		List<Users> usersList = objectMapper.readValue(userJSON, new TypeReference<List<Users>>() {
-		});
+        String goodsJSON = getJsonData("goods.json");
+        List<Goods> goodsList = objectMapper.readValue(goodsJSON, new TypeReference<List<Goods>>() {
+        });
+        goodsRepository.saveAll(goodsList);
 
-		usersRepository.saveAll(usersList);
+        String userJSON = getJsonData("users.json");
+        List<Users> usersList = objectMapper.readValue(userJSON, new TypeReference<List<Users>>() {
+        });
+        usersRepository.saveAll(usersList);
 
-	}
+
+//
+//		String categoryJSON = FileUtils.readFileToString(categoryResource.getFile(), "utf-8");
+//		List<Category> categoryList = objectMapper.readValue(categoryJSON, new TypeReference<List<Category>>() {
+//		});
+//		categoryRepository.saveAll(categoryList);
+//
+//		String menuJSON = FileUtils.readFileToString(menuResource.getFile(), "utf-8");
+//		List<Menu> menuList = objectMapper.readValue(menuJSON, new TypeReference<List<Menu>>() {
+//		});
+//		menuRepository.saveAll(menuList);
+//
+//		String goodsJSON = FileUtils.readFileToString(goodsResource.getFile(), "utf-8");
+//		List<Goods> goodsList = objectMapper.readValue(goodsJSON, new TypeReference<List<Goods>>() {
+//		});
+//
+//		goodsRepository.saveAll(goodsList);
+//
+//		String userJSON = FileUtils.readFileToString(usersResource.getFile(), "utf-8");
+//		List<Users> usersList = objectMapper.readValue(userJSON, new TypeReference<List<Users>>() {
+//		});
+//
+//		usersRepository.saveAll(usersList);
+
+    }
 
 }
